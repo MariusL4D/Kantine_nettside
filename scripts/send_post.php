076c1ddbdc87d3a54a947d77aea7dabe9bb891b7
MariusL4D <151638911+MariusL4D@users.noreply.github.com>
2023-11-22 23:18:33 +0100

    small fix

--- scripts/send_post.php
+++ scripts/send_post.php
@@ -4,9 +4,9 @@ $username = "root";
 $password = "";
 $dbname = "mo";
 
-// Check if the form has been submitted
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Assuming your form fields are named 'post_mo', 'post_content', and 'post_navn'
+
     $post_mo = $_POST['post_mo'];
     $post_content = $_POST['post_content'];
     $post_navn = $_POST['post_navn'];
@@ -18,19 +18,19 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $query = "INSERT INTO my_table(post_mo, post_content, post_navn) VALUES(:post_mo, :post_content, :post_navn)";
         $stmt = $conn->prepare($query);
 
-        // Bind parameters
+
         $stmt->bindParam(':post_mo', $post_mo);
         $stmt->bindParam(':post_content', $post_content);
         $stmt->bindParam(':post_navn', $post_navn);
 
-        // Execute the prepared statement
+
         $stmt->execute();
 
-        // Check if a record was successfully inserted
+
         if ($stmt->rowCount() > 0) {
-            // Redirect to a success page
+
             header("Location: ../mø_success.html");
-            exit(); // Ensure that no other output is sent
+            exit();
         } else {
             echo "Failed to insert record";
         }
@@ -38,4 +38,3 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         echo "SQL Error: " . $e->getMessage();
     }
 }
-?>
