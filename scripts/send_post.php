fd98655b27fe79e795063204b5ebb553ec2bc6a7
MariusL4D <151638911+MariusL4D@users.noreply.github.com>
2023-11-22 23:15:48 +0100

    Fixed PHP code
    Fixed it so the php code actually worked and posted the submitted stuff in the form to the database. One step closer to being done with the site

--- scripts/send_post.php
+++ scripts/send_post.php
@@ -1,23 +1,41 @@
 <?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "mo";
+
+// Check if the form has been submitted
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Fetch form data
-    $middagsønske = $_POST['post_mø'];
-    $forklaring = $_POST['post_content'];
-    $navn = $_POST['post_navn'];
+    // Assuming your form fields are named 'post_mo', 'post_content', and 'post_navn'
+    $post_mo = $_POST['post_mo'];
+    $post_content = $_POST['post_content'];
+    $post_navn = $_POST['post_navn'];
 
-    // Email information
-    $to = "marius.larsson1@gmail.com"; // Replace with your email address
-    $subject = "Middagsønske from $navn";
-    $message = "Middagsønske: $middagsønske\n\nForklaring av middag: $forklaring\n\nNavn: $navn";
+    try {
+        $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    // Send email
-    $headers = "From: marius.larsson1@gmail.com"; // Replace with a valid sender email address
-    if (mail($to, $subject, $message, $headers)) {
-        // Email sent successfully, redirect to a new page
-        header("Location: success.html"); // Replace with the URL of the success page
-        exit(); // Ensure that no other code runs after the redirection
-    } else {
-        echo "<p>Oops! Something went wrong. Please try again later.</p>";
+        $query = "INSERT INTO my_table(post_mo, post_content, post_navn) VALUES(:post_mo, :post_content, :post_navn)";
+        $stmt = $conn->prepare($query);
+
+        // Bind parameters
+        $stmt->bindParam(':post_mo', $post_mo);
+        $stmt->bindParam(':post_content', $post_content);
+        $stmt->bindParam(':post_navn', $post_navn);
+
+        // Execute the prepared statement
+        $stmt->execute();
+
+        // Check if a record was successfully inserted
+        if ($stmt->rowCount() > 0) {
+            // Redirect to a success page
+            header("Location: ../mø_success.html");
+            exit(); // Ensure that no other output is sent
+        } else {
+            echo "Failed to insert record";
+        }
+    } catch (PDOException $e) {
+        echo "SQL Error: " . $e->getMessage();
     }
 }
 ?>
